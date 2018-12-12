626dc916eab76977bfa0a4b97f22c1956f45f225
Manas1524 <45575408+Manas1524@users.noreply.github.com>
2018-12-12 14:13:57 -0600

    Add files via upload

--- BackEndGarbo.java
+++ BackEndGarbo.java
@@ -7,29 +7,42 @@ import java.util.*;
 import javax.swing.*;
 
 public class BackEndGarbo {
-	public JPanel questionPanel(ArrayList<Question> questionList, String category, String pointValue) {
+	public JPanel questionPanel(ArrayList<Question> questionList, String category, int pointValue) {
 		JPanel questionPanel = new JPanel();
+		String question = "";
+		
 		for(int i = 0; i<questionList.size(); i++) {
 			Question questionStats = questionList.get(i);
 			if(questionStats.getCategory().equals(category) && questionStats.getPoints() == pointValue) {
-				
+				question = questionStats.getQuestion();
 			}
 		}
 		
+		JLabel question1 = new JLabel(question);
 		JLabel cat1 = new JLabel(category);
-		JLabel points = new JLabel(pointValue);
+		JLabel points = new JLabel("" + pointValue);
+		JTextArea answerField = new JTextArea("Enter Answer Here");
+		JButton enterAnswer = new JButton("Enter Answer");
 		
 		questionPanel.add(cat1);
 		questionPanel.add(points);
-	}
-	public boolean enterAnswer(String userAnswer, ArrayList<Question> questionList, String category, int pointValue) {
-		for(int i = 0; i<questionList.size(); i++) {
-			Question questionStats = questionList.get(i);
-			if(questionStats.getCategory.equals(category)) {
-				
-			}
-		}
+		questionPanel.add(question1);
+		questionPanel.add(answerField);
+		questionPanel.add(enterAnswer);
 		
-	public boolean
+		enterAnswer.addActionListener(new ActionListener()
+		{
+			  public void actionPerformed(ActionEvent e)
+			  {
+				  for(int i = 0; i<questionList.size(); i++) {
+						Question questionStats = questionList.get(i);
+						if(questionStats.getCategory().equals(category)) {
+							
+						}
+			  }
+			}
+		});
+		return questionPanel;
 	}
 }
+
